62cce24281e9c50ffa45827f84fd2405366763c5
idrisaltamimi <97464844+idrisaltamimi@users.noreply.github.com>
2022-06-11 08:08:26 +0400

    Add files via upload

--- filmCard.js
+++ filmCard.js
@@ -42,9 +42,9 @@ export function filmCard(data) {
       if (Plot?.length < 134) {
          readmoreHtml.style.display = "none"
          plotHtml.textContent = Plot === "N/A" ? "" : Plot
-      } else if((Plot?.length >= 134) {
-         let displayText = Plot?.slice(0, 132)
-         let moreText = Plot?.slice(132)
+      } else {
+         let displayText = Plot.slice(0, 132)
+         let moreText = Plot.slice(132)
          plotHtml.textContent = displayText.concat("...")
 
          readmoreHtml.addEventListener("click", () => showmoreLess(moreText, "none", "block"))
